fb7882951ceb8dd6ca99e2d0f7ceffa591837c9f
Craig Petzel <cpetzel@squareup.com>
2022-11-09 13:23:04 -0800

    First go at CI

---
+++ scripts/ci-build.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+unset DISPLAY
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REPO_DIR=$(dirname "$SCRIPT_DIR")
+ANDROID_HOME="$HOME/android-sdk"
+SHARED_CACHE="/mnt/nfs/shared-cache/android/cash"
+GW_PROPERTIES="$REPO_DIR/gradle/wrapper/gradle-wrapper.properties"
+
+set -e
+
+if [[ "$TEST_RUNNER" == "on_success" ]]; then
+  scripts/on-success
+  exit
+fi
+
+export ANDROID_HOME
+# Workaround for https://issuetracker.google.com/issues/148189425
+rm -rf "$ANDROID_HOME"/ndk*
+
+echo "sdk.dir=$ANDROID_HOME" >local.properties
+
+mkdir -p "$ANDROID_HOME/licenses" || true
+echo -e "24333f8a63b6825ea9c5514f83c2829b004d1fee" >"$ANDROID_HOME/licenses/android-sdk-license"
+
+# On local builds, use gradle-all for debuggable sources.
+# On CI builds, use gradle-bin for faster downloading.
+echo "Swapping gradle-all for gradle-bin in ${GW_PROPERTIES}"
+sed -i 's/-all/-bin/g' "${GW_PROPERTIES}"
+
+####################################################################################################
+#### MAIN BUILD
+####################################################################################################
+# Swap the Gradle properties -- which are tailored for local builds -- with a CI-specific config.
+rm gradle.properties
+mv gradle.properties.kochiku gradle.properties
+
+# Point the build cache onto the shared NFS drive so it can be reused by all workers.
+# echo "android.buildCacheDir=$SHARED_CACHE/build-cache" >> gradle.properties
+
+# Set nonProxyHosts in gradle.properties so CI can access maven.global.square without going through cloudproxy
+echo "systemProp.https.nonProxyHosts=*.square|*.squareup.com|*.sqcorp.co" >>gradle.properties
+echo "systemProp.http.nonProxyHosts=*.square|*.squareup.com|*.sqcorp.co" >>gradle.properties
+
+buildDebug() {
+  JAVA_OPTS="-Xmx2048M" ./gradlew sample-app:assembleDebug --no-daemon
+}
+
+publishDebug() {
+  echo "publishing"
+  pwd
+
+  source $PROJECT_ROOT/scripts/ci/mobile-releases.sh
+
+  # For signing with release cert
+  # sign_apk "android/app/build/outputs/apk/release/app-release.apk" "android/app/build/outputs/apk/release/app-release-signed.apk"
+  upload_artifact "sample-app/build/outputs/apk/debug/sample-app-debug.apk" "debug" "apk" "$PACKAGE_VERSION"
+
+  # we are publishing when uploading. no need to set visibility
+  # publish_artifact "release" "$GIT_COMMIT"
+}
+
+echo "Running ${TEST_RUNNER}..."
+
+# TEST_RUNNER cases should match targets.type in kochiku.yml
+case "${TEST_RUNNER}" in
+buildDebug)
+  buildDebug
+  ;;
+publishDebug)
+  publishDebug
+  ;;
+*)
+  echo "Unknown TEST_RUNNER value: ${TEST_RUNNER}"
+  exit 1
+  ;;
+esac
---
+++ scripts/mobile-releases.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+SECRETS_DIR="/data/app/kochiku-worker/secrets"
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+# Executed by APK-building shards on 'kochiku-worker' (EC2 workers)
+function sign_apk() {
+  UNSIGNED_APK="$1"
+  SIGNED_APK="$2"
+
+  echo "======================"
+  echo "Signing APK"
+  echo "======================"
+
+  curl \
+    -sS \
+    -L \
+    -v \
+    --fail \
+    --connect-timeout 120 \
+    --max-time 120 \
+    --cacert "$SECRETS_DIR/service2service.ca.pem" \
+    --cert-type 'pem' \
+    --cert "$SECRETS_DIR/kochiku-worker-s2s.crt" \
+    --key "$SECRETS_DIR/kochiku-worker-s2s.key" \
+    -o "$SIGNED_APK" \
+    -X 'POST' \
+    -H "X-Speleo-Trace-Id: $(uuidgen | sed 's/\-//g')" \
+    -F 'organization=square' \
+    -F "apk_data=@$UNSIGNED_APK" \
+    -F 'v2=true' \
+    'https://apk-signer.global.square/sign-apk'
+
+  if [ $? -ne 0 ]; then
+    echo "Error signing '$UNSIGNED_APK' with apksigner"
+  else
+    echo "Signed '$UNSIGNED_APK' with apksigner"
+  fi
+}
+
+function sign_aab() {
+  UNSIGNED_AAB="$1"
+  SIGNED_AAB="$2"
+
+  echo "======================"
+  echo "Signing AAB"
+  echo "======================"
+
+  curl \
+    -sS \
+    -L \
+    -v \
+    --fail \
+    --connect-timeout 120 \
+    --max-time 120 \
+    --cacert "$SECRETS_DIR/service2service.ca.pem" \
+    --cert-type 'pem' \
+    --cert "$SECRETS_DIR/kochiku-worker-s2s.crt" \
+    --key "$SECRETS_DIR/kochiku-worker-s2s.key" \
+    -o "$SIGNED_AAB" \
+    -X 'POST' \
+    -H "X-Speleo-Trace-Id: $(uuidgen | sed 's/\-//g')" \
+    -F 'organization=square' \
+    -F "aab_data=@$UNSIGNED_AAB" \
+    'https://apk-signer.global.square/sign-aab'
+
+  if [[ $? -ne 0 ]]; then
+    echo "Error signing '$UNSIGNED_AAB' with apksigner"
+  else
+    echo "Signed '$UNSIGNED_AAB' with apksigner"
+  fi
+}
+
+# Executed by artifact-building shards on 'kochiku-worker' (EC2 workers)
+function upload_artifact() {
+  SIGNED_ARTIFACT_PATH="$1"
+  VARIANT="$2"
+  ARTIFACT_TYPE="$3"
+
+  echo "======================"
+  echo "Uploading artifact"
+  echo "======================"
+
+  # if [[ "$GIT_BRANCH" == "master" ]]; then
+  VERSION="master"
+  # else
+  # VERSION=$(echo "$GIT_BRANCH" | sed -n 's/^release\///p')
+  # fi
+
+  echo "Setting version for upload artifact to '$VERSION'"
+
+  curl \
+    -sS \
+    -L \
+    -v \
+    --fail \
+    --connect-timeout 20 \
+    --max-time 100 \
+    --cacert "$SECRETS_DIR/service2service.ca.pem" \
+    --cert-type 'pem' \
+    --cert "$SECRETS_DIR/kochiku-worker-s2s.crt" \
+    --key "$SECRETS_DIR/kochiku-worker-s2s.key" \
+    -X 'POST' \
+    -H "X-Speleo-Trace-Id: $(uuidgen | sed 's/\-//g')" \
+    -F 'application=cash-app-pay-sandbox' \
+    -F 'platform=android' \
+    -F "version=$VERSION" \
+    -F "sha=$GIT_COMMIT" \
+    -F "variant=$VARIANT" \
+    -F "hidden=false" \
+    -F "artifact_data=@$SIGNED_ARTIFACT_PATH" \
+    -F "file_size=$(wc -c <$SIGNED_ARTIFACT_PATH | tr -d ' ')" \
+    'https://mobile-releases.global.square/api/upload-artifact'
+
+  if [ $? -ne 0 ]; then
+    echo "Error uploading '$SIGNED_ARTIFACT_PATH' to mobile-releases"
+  else
+    echo "Uploaded '$SIGNED_ARTIFACT_PATH' to mobile-releases"
+  fi
+}
