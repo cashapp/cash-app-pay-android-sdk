79466e24653699db36cefa7d7e111fe3bc3364fb
Pedro Veloso <pedroveloso@squareup.com>
2022-12-07 16:47:47 -0800

    Add unit tests for Integration Exceptions conditions

---
+++ paykit/src/test/java/com/squareup/cash/paykit/FakeData.kt
@@ -0,0 +1,7 @@
+package com.squareup.cash.paykit
+
+object FakeData {
+  const val CLIENT_ID = "fake_client_id"
+  const val BRAND_ID = "fake_brand_id"
+  const val REDIRECT_URI = "fake_redirect_uri"
+}
