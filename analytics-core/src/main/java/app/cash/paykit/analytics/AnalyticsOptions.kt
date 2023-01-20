40dca672d2fdfeb7870cbc85ff8a749e57527204
Craig Petzel <cpetzel@squareup.com>
2023-01-20 13:24:36 -0800

    First draft of analytic events. persistence only
    remove unused dependencies
    
    lint everything
    
    build gradle changes for linting and unifying versions with core libs
    
    PR feedback
    
    remove test

---
+++ analytics-core/src/main/java/app/cash/paykit/analytics/AnalyticsOptions.kt
@@ -0,0 +1,64 @@
+package app.cash.paykit.analytics
+
+import android.util.Log
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+class AnalyticsOptions(
+  /** Delay in seconds to wait until we begin to deliver events. */
+  val delay: Duration = 0.seconds,
+
+  /** Interval of time between uploading batches. */
+  val interval: Duration = 30.seconds,
+
+  /** Number of packages to include per process. */
+  val maxPackageCountPerProcess: Int = 30,
+
+  /** Number of events to include in a given batch process. */
+  val batchSize: Int = 10,
+
+  /** The name of the database file on disk. */
+  val databaseName: String = "analytic-packages.db",
+
+  /** The log level. */
+  val logLevel: Int = Log.ERROR,
+
+  /** whether or not logging is disabled */
+  val isLoggerDisabled: Boolean = false,
+
+  /** The version code of the application. */
+  val applicationVersionCode: Int = 0,
+) {
+
+  internal constructor(builder: Builder) : this(
+    builder.delay,
+    builder.interval,
+    builder.maxPackageCountPerProcess,
+    builder.batchSize,
+    builder.databaseName,
+    builder.logLevel,
+    builder.isLoggerDisabled,
+    builder.applicationVersionCode,
+  )
+
+  companion object {
+    inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
+  }
+
+  class Builder {
+    var delay: Duration = 0.seconds
+    var interval: Duration = 30.seconds
+    var maxPackageCountPerProcess: Int = 30
+    var batchSize: Int = 10
+    var databaseName: String = "analytic-events.db"
+    var logLevel: Int = Log.ERROR
+    var isLoggerDisabled: Boolean = false
+    var applicationVersionCode: Int = 0
+
+    fun build(): AnalyticsOptions {
+      check(interval.isPositive()) { "interval must be > 0" }
+      check(!delay.isNegative()) { "delay must be >= 0" }
+      return AnalyticsOptions(this)
+    }
+  }
+}
