be86a8328f2a3e8a4b26a2c2f83270564d81b053
Pedro Veloso <pedroveloso@squareup.com>
2023-04-24 14:42:46 -0700

    Code cleanup (and research on proguard rules being handed to consumers) (#98)
    * Add proguard rules to project for automatic client consumption
    
    * Fix constant name after renaming class
    
    * Concrete R8 rules are actually not needed

--- core/src/main/java/app/cash/paykit/core/analytics/AnalyticsEventStream2Event.kt
+++ core/src/main/java/app/cash/paykit/core/analytics/AnalyticsEventStream2Event.kt
@@ -20,13 +20,13 @@ import app.cash.paykit.analytics.core.Deliverable
 /**
  * Class that represents the payload to be delivered to the ES2 API.
  */
-internal data class EventStream2Event constructor(
+internal data class AnalyticsEventStream2Event constructor(
   override val content: String,
 ) : Deliverable {
   override val type = ESEventType
   override val metaData = null
 
   companion object {
-    const val ESEventType = "EventStream2Event"
+    const val ESEventType = "AnalyticsEventStream2Event"
   }
 }
